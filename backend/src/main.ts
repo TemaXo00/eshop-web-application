ce1bba03142d133a87aab0d29f31ed12b4008adb
TemaXo <melnikov.artem294@gmail.com>
2025-12-20 21:32:25 +0300

    [feat] Added Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('ESHop API')
+      .setVersion('1.0.0')
+      .setDescription('ESHop API for MITSO Laboratories')
+      .setContact('TemaXo00', 'https://github.com/TemaXo00/', 'melnikov.artem294@gmail.com')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    ignoreGlobalPrefix: true,
+    deepScanRoutes: true,
+  });
+
+  SwaggerModule.setup('/docs', app, document, {
+    explorer: true,
+    customSiteTitle: 'ESHop API Documentation',
+    jsonDocumentUrl: "docs/swagger.json",
+    yamlDocumentUrl: "docs/swagger.yaml",
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
