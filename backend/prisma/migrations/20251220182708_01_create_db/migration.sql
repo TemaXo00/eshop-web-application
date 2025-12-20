d8446d6b7951d0a72e7f2e326010fbf02558f75d
TemaXo <melnikov.artem294@gmail.com>
2025-12-20 21:32:43 +0300

    [feat] Created Prisma migrations

---
+++ backend/prisma/migrations/20251220182708_01_create_db/migration.sql
@@ -0,0 +1,302 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('USER', 'EMPLOYEE', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'BANNED', 'HIRED');
+
+-- CreateEnum
+CREATE TYPE "StorePosition" AS ENUM ('SELLER', 'MANAGER', 'ADMINISTRATOR', 'DIRECTOR', 'CONSULTANT');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethods" AS ENUM ('CARD', 'CASH');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('OK', 'DECLINED', 'REFUND');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30),
+    "password_hash" TEXT NOT NULL,
+    "username" VARCHAR(30) NOT NULL,
+    "avatar_url" TEXT,
+    "phone" VARCHAR(20),
+    "email" VARCHAR(60) NOT NULL,
+    "role" "Roles" NOT NULL DEFAULT 'USER',
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "store_id" INTEGER,
+    "position" "StorePosition",
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "stores" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "address" VARCHAR(70) NOT NULL,
+    "email" VARCHAR(60) NOT NULL,
+    "store_image" TEXT,
+    "opening_time" TIME NOT NULL,
+    "closing_time" TIME NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stores_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "suppliers" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "email" VARCHAR(60) NOT NULL,
+    "rating" DECIMAL(2,1) NOT NULL,
+    "logo_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "suppliers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" TEXT,
+    "image_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" TEXT,
+    "price" INTEGER NOT NULL,
+    "rating" DECIMAL(2,1) NOT NULL,
+    "images" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "stock" (
+    "id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "store_id" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "reserved_quantity" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sales" (
+    "id" SERIAL NOT NULL,
+    "customer_id" INTEGER NOT NULL,
+    "seller_id" INTEGER NOT NULL,
+    "store_id" INTEGER NOT NULL,
+    "total_amount" DECIMAL(10,2) NOT NULL,
+    "payment_method" "PaymentMethods" NOT NULL,
+    "payment_status" "PaymentStatus" NOT NULL DEFAULT 'OK',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sales_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sale_items" (
+    "id" SERIAL NOT NULL,
+    "sale_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price_at_sale" DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT "sale_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(70) NOT NULL,
+    "description" TEXT,
+    "liked" TEXT,
+    "disliked" TEXT,
+    "images" TEXT[],
+    "user_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "rating" DECIMAL(2,1) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToProduct" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_CategoryToProduct_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_ProductToSupplier" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ProductToSupplier_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_store_id_idx" ON "users"("store_id");
+
+-- CreateIndex
+CREATE INDEX "users_role_idx" ON "users"("role");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "stores_address_key" ON "stores"("address");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "stores_email_key" ON "stores"("email");
+
+-- CreateIndex
+CREATE INDEX "stores_address_idx" ON "stores"("address");
+
+-- CreateIndex
+CREATE INDEX "stores_email_idx" ON "stores"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "suppliers_phone_key" ON "suppliers"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "suppliers_email_key" ON "suppliers"("email");
+
+-- CreateIndex
+CREATE INDEX "suppliers_rating_idx" ON "suppliers"("rating");
+
+-- CreateIndex
+CREATE INDEX "products_rating_idx" ON "products"("rating");
+
+-- CreateIndex
+CREATE INDEX "products_created_at_idx" ON "products"("created_at");
+
+-- CreateIndex
+CREATE INDEX "products_price_idx" ON "products"("price");
+
+-- CreateIndex
+CREATE INDEX "stock_product_id_idx" ON "stock"("product_id");
+
+-- CreateIndex
+CREATE INDEX "stock_store_id_idx" ON "stock"("store_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "stock_product_id_store_id_key" ON "stock"("product_id", "store_id");
+
+-- CreateIndex
+CREATE INDEX "sales_customer_id_idx" ON "sales"("customer_id");
+
+-- CreateIndex
+CREATE INDEX "sales_seller_id_idx" ON "sales"("seller_id");
+
+-- CreateIndex
+CREATE INDEX "sales_store_id_idx" ON "sales"("store_id");
+
+-- CreateIndex
+CREATE INDEX "sales_created_at_idx" ON "sales"("created_at");
+
+-- CreateIndex
+CREATE INDEX "sales_total_amount_idx" ON "sales"("total_amount");
+
+-- CreateIndex
+CREATE INDEX "sale_items_sale_id_idx" ON "sale_items"("sale_id");
+
+-- CreateIndex
+CREATE INDEX "sale_items_product_id_idx" ON "sale_items"("product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sale_items_sale_id_product_id_key" ON "sale_items"("sale_id", "product_id");
+
+-- CreateIndex
+CREATE INDEX "reviews_product_id_idx" ON "reviews"("product_id");
+
+-- CreateIndex
+CREATE INDEX "reviews_rating_idx" ON "reviews"("rating");
+
+-- CreateIndex
+CREATE INDEX "reviews_created_at_idx" ON "reviews"("created_at");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_user_id_product_id_key" ON "reviews"("user_id", "product_id");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToProduct_B_index" ON "_CategoryToProduct"("B");
+
+-- CreateIndex
+CREATE INDEX "_ProductToSupplier_B_index" ON "_ProductToSupplier"("B");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stock" ADD CONSTRAINT "stock_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "stock" ADD CONSTRAINT "stock_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales" ADD CONSTRAINT "sales_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales" ADD CONSTRAINT "sales_seller_id_fkey" FOREIGN KEY ("seller_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sales" ADD CONSTRAINT "sales_store_id_fkey" FOREIGN KEY ("store_id") REFERENCES "stores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale_items" ADD CONSTRAINT "sale_items_sale_id_fkey" FOREIGN KEY ("sale_id") REFERENCES "sales"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale_items" ADD CONSTRAINT "sale_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToProduct" ADD CONSTRAINT "_CategoryToProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToProduct" ADD CONSTRAINT "_CategoryToProduct_B_fkey" FOREIGN KEY ("B") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductToSupplier" ADD CONSTRAINT "_ProductToSupplier_A_fkey" FOREIGN KEY ("A") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductToSupplier" ADD CONSTRAINT "_ProductToSupplier_B_fkey" FOREIGN KEY ("B") REFERENCES "suppliers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
